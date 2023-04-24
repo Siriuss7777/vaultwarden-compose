047ba9e204091a89920b1ef2908aa042936fa3f0
root <root@PC-IUT>
2023-04-24 12:13:42 +0200

    Edited docker-compose.yml

--- pull-vault.sh
+++ pull-vault.sh
@@ -1,5 +1,7 @@
 cd vaultwarden
 
+mkdir data
+
 wget https://github.com/dani-garcia/bw_web_builds/releases/download/v2023.3.0b/bw_web_v2023.3.0b.tar.gz https://github.com/dani-garcia/bw_web_builds/releases/download/v2023.3.0b/bw_web_v2023.3.0b.tar.gz.asc
 
 tar xvf bw_web_v2023.3.0b.tar.gz
